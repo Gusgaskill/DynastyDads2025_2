0a2406d5ffca1846b220b07124fe5ae29dd610bb
Gusgaskill <138895338+Gusgaskill@users.noreply.github.com>
2025-01-12 13:58:01 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
-export const leagueID = "1062784235112607744"; // your league ID
-export const leagueName = "MFFL"; // your league name
-export const dues = 75; // (optional) used in template constitution page
+export const leagueID = "1180715519148888064"; // your league ID
+export const leagueName = "Dynasty Dads 2"; // your league name
+export const dues = 20; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
